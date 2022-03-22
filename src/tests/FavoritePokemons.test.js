edc28bea8c29524769dfcf74b91b0d002d7d926a
ítalo Lima <contatoitallo@hotmail.com>
2022-03-22 18:42:36 -0300

    Testa o componente <About.js />-Req 2()

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import { Router, MemoryRouter } from 'react-router-dom';
-// import { createMemoryHistory } from 'history';
-import About from '../components/About';
-import renderWithRouter from '../renderWithRouter';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// // import userEvent from '@testing-library/user-event';
+// // import { Router, MemoryRouter } from 'react-router-dom';
+// // import { createMemoryHistory } from 'history';
+// import About from '../components/About';
+// import renderWithRouter from '../renderWithRouter';
 
-describe('Testando o Componente Favorite', () => {
-  test('', () => {});
-});
+// describe('Testando o Componente Favorite', () => {
+//   test('', () => {});
+// });
