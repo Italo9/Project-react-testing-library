3e8da21aaff2be8ca7e835849f1474398ab7471e
ítalo Lima <contatoitallo@hotmail.com>
2022-03-23 16:19:39 -0300

    Testa o componente <FavoritePokemons.js />-Req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,20 +1,24 @@
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// // import userEvent from '@testing-library/user-event';
-// // import { Router, MemoryRouter } from 'react-router-dom';
-// // import { createMemoryHistory } from 'history';
-// // import About from '../components/About';
-// import renderWithRouter from '../renderWithRouter';
-// import { FavoritePokemons } from '../components';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { Router, MemoryRouter } from 'react-router-dom';
+// import { createMemoryHistory } from 'history';
+// import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+import pokemons from '../data';
 
-// describe('Testando o Componente Favorite', () => {
-//   test('Verifica se a pessoa não tiver pokémons favoritos', () => {
-//     renderWithRouter(<FavoritePokemons />);
-//     const notFavoritePok = screen.queryByText(/No favorite pokemon found/i);
-//     expect(notFavoritePok).toBeDefined();
-//   });
-//   test('Verifica se é exibido todos os cards de pokémons favoritados', () => {
-//     const cardsPokemonFavori = screen.queryAllByRole('dialog');
-//     expect(cardsPokemonFavori).toBe(cardsPokemonFavori.length);
-//   });
-// });
+describe('Testando o Componente Favorite', () => {
+  test('Verifica se a pessoa não tiver pokémons favoritos', () => {
+    const pokemon = [];
+    renderWithRouter(<FavoritePokemons pokemons={ pokemon } />);
+    const notFavoritePok = screen.queryByText(/No favorite pokemon found/i);
+    expect(notFavoritePok).toBeInTheDocument();
+  });
+  test('Verifica se é exibido todos os cards de pokémons favoritados', () => {
+    const dataPokemons = pokemons;
+    renderWithRouter(<FavoritePokemons pokemons={ dataPokemons } />);
+    const cardsPokemonFavori = screen.getByText(/Pikachu/i);
+    expect(cardsPokemonFavori).toBeInTheDocument();
+  });
+});
